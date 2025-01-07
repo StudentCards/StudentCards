f67c9977d6984356dc2b6ede03b92a29e44bb297
Oliwia Strzelec <strolliw@gmail.com>
2025-01-07 21:46:15 +0100

    add manage button and upgrade styles

--- src/components/Set.jsx
+++ src/components/Set.jsx
@@ -1,15 +1,22 @@
 import { Link } from 'react-router-dom';
 const Set = ({ id, title, description }) => {
+	const btnClasses = 'absolute bottom-0  px-2 py-1 rounded-md uppercase text-white bg-indigo-500 hover:bg-indigo-400 hover:scale-95 transition-all'
 	return (
 		<div className='relative w-full h-full pb-12 rounded-md bg-indigo-100 text-black'>
 			<h3 className='text-xl mb-4 tracking-wide font-medium'>{title}</h3>
 			<p>{description}</p>
-			
+
 			<Link
 				to={`/play/${id}`}
-				className='absolute bottom-0 right-0 px-4 py-2 rounded-md text-white bg-indigo-500 hover:bg-indigo-400 hover:scale-95 transition-all'
+				className={`${btnClasses} right-0`}
 			>
-				PLAY
+				play
+			</Link>
+			<Link
+				to={`/manageSet/${id}`}
+				className={`${btnClasses} right-16`}
+			>
+				manage
 			</Link>
 		</div>
 	);
