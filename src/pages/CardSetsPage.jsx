6537b4517a11390ff48b08b76f43e67251f10b54
Oliwia Strzelec <strolliw@gmail.com>
2025-01-07 21:47:39 +0100

    implement create new set form

--- src/pages/CardSetsPage.jsx
+++ src/pages/CardSetsPage.jsx
@@ -1,11 +1,12 @@
 import CardSets from '../components/CardSets.jsx';
-import { useState, useEffect} from 'react';
-
-import {getCardSets, getPublicCardSets} from '../api/set-api.js';
-
+import { useState, useEffect } from 'react';
+import { getCardSets, getPublicCardSets } from '../api/set-api.js';
+import CreateSetModal from '../components/CreateSetModal.jsx';
 
 const CardSetsPage = () => {
 	const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
 	const [cardSets, setCardSets] = useState([]);
 	const [publicCardSets, setPublicCardSets] = useState([]);
 
@@ -41,17 +42,39 @@ const CardSetsPage = () => {
 		}
 	};
 
+	const handleCreateNewSet = e => {
+		e.preventDefault();
+
+		const formData = new FormData(event.target);
+		const title = formData.get('title');
+		const description = formData.get('description');
+		// is ID generation needed here or it's just next number added in database ?
+
+		// send data to backend
+
+		setIsModalOpen(false);
+	};
+
 	return (
-		<main className='text-white my-10'>
+		<main className='my-10'>
 			{isUserLoggedIn && (
 				<section className='relative bg-indigo-700 rounded-md pb-16'>
-					<CardSets title='Your card sets' cardSets={cardSets} />
-					<button className='absolute right-5 px-4 p-2 rounded-md transition-all hover:scale-105 hover:bg-indigo-300 text-indigo-950 bg-indigo-200'>
+					<CardSets title='Your private card sets' cardSets={cardSets} />
+					<button
+						onClick={() => setIsModalOpen(true)}
+						className='absolute right-5 px-4 p-2 rounded-md transition-all hover:scale-105 hover:bg-indigo-300 text-indigo-950 bg-indigo-200'
+					>
 						Create new card set
 					</button>
 				</section>
 			)}
 			<CardSets title='Public card sets' cardSets={publicCardSets} />
+			{isModalOpen && (
+				<CreateSetModal
+					handleCreateNewSet={handleCreateNewSet}
+					setIsModalOpen={setIsModalOpen}
+				/>
+			)}
 		</main>
 	);
 };
