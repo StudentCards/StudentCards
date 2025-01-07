8e2a33add1209d724d112310c679043657cd5941
qsqus <149406771+qsqus@users.noreply.github.com>
2025-01-07 20:00:40 +0100

    add management command that populates the database,  adjust public sets url

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ const BASE_URL = 'http://127.0.0.1:8000/';
 
 export const FLASHCARDS_URL = `${BASE_URL}flashcards/`;
 export const FLASHCARD_SETS_URL = `${BASE_URL}flashcard-sets/`;
-export const PUBLIC_FLASHCARD_SETS_URL = `${FLASHCARD_SETS_URL}public`;
+export const PUBLIC_FLASHCARD_SETS_URL = `${FLASHCARD_SETS_URL}public/`;
 
 export const apiCall = async(func, returnResponseData=false) => {
     try {
