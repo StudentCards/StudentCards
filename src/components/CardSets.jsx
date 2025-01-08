96898b62e05ada4890431e6c6362b1a7c3dc05b7
Oliwia Strzelec <strolliw@gmail.com>
2025-01-08 20:28:35 +0100

    Handle loading state for card fetching to improve UX

--- src/components/CardSets.jsx
+++ src/components/CardSets.jsx
@@ -1,11 +1,19 @@
 import Set from '../components/Set.jsx';
 
-const CardSets = ({ title, cardSets }) => {
+const CardSets = ({ title, cardSets, isLoading }) => {
+	const infoStyles = 'border-b p-4 text-gray-300 text-center';
 	return (
-		<main className='mx-10 p-4 text-white my-10'>
+		<main className='mx-10 p-4 text-white my-10 '>
 			<h1 className='text-2xl sm:text-3xl md:text-4xl font-bold mb-6 '>
 				{title}
 			</h1>
+			{!isLoading && cardSets.length == 0 && (
+				<p className={infoStyles}>
+					There are no sets available yet or you have lost connection to
+					database.
+				</p>
+			)}
+			{isLoading && <p className={infoStyles}>Fetching flashcards...</p>}
 			<ul className='mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8'>
 				{cardSets.map(set => (
 					<li
--- src/pages/CardSetsPage.jsx
+++ src/pages/CardSetsPage.jsx
@@ -1,30 +1,31 @@
 import CardSets from '../components/CardSets.jsx';
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
-import {getCardSets, getPublicCardSets} from '../api/set-api.js';
-
+import { getCardSets, getPublicCardSets } from '../api/set-api.js';
 
 const CardSetsPage = () => {
 	const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
 	const [cardSets, setCardSets] = useState([]);
 	const [publicCardSets, setPublicCardSets] = useState([]);
+	const [isLoading, setIsLoading] = useState(false)
 
 	useEffect(() => {
+		setIsLoading(true)
 		const token = localStorage.getItem('authToken');
 		if (token) {
 			setIsUserLoggedIn(true);
 			fetchCardSets(token);
-		}
-		else setIsUserLoggedIn(false);
+		} else setIsUserLoggedIn(false);
 
 		fetchPublicCardSets();
+		setIsLoading(false)
 	}, []);
 
 	const fetchCardSets = async token => {
 		const response = await getCardSets(token);
-		
+
 		if (response.success) {
-			setCardSets(response.data || [])
+			setCardSets(response.data || []);
 		} else {
 			// setError(response.errorMessage);
 		}
@@ -35,7 +36,6 @@ const CardSetsPage = () => {
 
 		if (response.success) {
 			setPublicCardSets(response.data || []);
-
 		} else {
 			// setError(response.errorMessage);
 		}
@@ -45,13 +45,21 @@ const CardSetsPage = () => {
 		<main className='text-white my-10'>
 			{isUserLoggedIn && (
 				<section className='relative bg-indigo-700 rounded-md pb-16'>
-					<CardSets title='Your card sets' cardSets={cardSets} />
+					<CardSets
+						isLoading={isLoading}
+						title='Your card sets'
+						cardSets={cardSets}
+					/>
 					<button className='absolute right-5 px-4 p-2 rounded-md transition-all hover:scale-105 hover:bg-indigo-300 text-indigo-950 bg-indigo-200'>
 						Create new card set
 					</button>
 				</section>
 			)}
-			<CardSets title='Public card sets' cardSets={publicCardSets} />
+			<CardSets
+				isLoading={isLoading}
+				title='Public card sets'
+				cardSets={publicCardSets}
+			/>
 		</main>
 	);
 };
