1406986b097e7e97341244430a4be387793aa3eb
Kamil Marszałek <108593228+KamilMarshal@users.noreply.github.com>
2025-01-12 15:47:01 +0100

    Delete unused code

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -15,14 +15,3 @@ class FlashcardSetSerializer(serializers.ModelSerializer):
         model = FlashcardSet
         fields = ['id', 'title', 'description', 'is_public', 'owner']
         read_only_fields = ['owner']
-
-
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'password']
-
-    def validate_username(self, value):
-        if User.objects.filter(username=value).exists():
-            raise serializers.ValidationError("Username already taken")
-        return value
